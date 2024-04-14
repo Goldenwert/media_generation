8c1f3854902bc2a5b205e765cf95b5772929c986
Narodny <art.gari@yandex.ru>
2024-04-14 15:34:10 +0200

    Update url_json_extractor.py
    templated path

--- url_json_extractor.py
+++ url_json_extractor.py
@@ -1,16 +1,14 @@
 import pandas as pd
 import json
 
-# Load the CSV file
-data = pd.read_csv('C:/Users/garim/OneDrive/Рабочий стол/project/skincare_trends_articles.csv')
+#load data contained article
+data = pd.read_csv('set_path_of_aricles.csv')
 
-# Extract URLs into a list
+#urls extracted and prepared as .json with semicolon as a sep
 urls = data['URL'].tolist()
-
-# Prepare JSON data
 json_data = {'urls': urls}
 
-# Write to a JSON file
+#file written
 with open('urls.json', 'w') as json_file:
     json.dump(json_data, json_file)
 
